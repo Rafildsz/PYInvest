9e593767727bc931e50b6a8d712db7a5c5777094
RafaelCespedes-br <rafael.cespedes@ibm.com>
2026-01-28 15:28:18 -0300

    Finishing clients and investment gateway

--- investment_gateway_service/app/investments/investments.py
+++ investment_gateway_service/app/investments/investments.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 import requests
 
 router = APIRouter(tags=["Investimentos"])
@@ -9,7 +9,11 @@ DATA_SERVICE_URL = "http://127.0.0.1:8001/internal/investimentos"
 @router.post("/api/v1/investimentos/")
 def criar_investimento(investimento: dict):
     response = requests.post(DATA_SERVICE_URL, json=investimento)
-    response.raise_for_status()
+    if response.status_code >= 400:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail=response.json().get("detail")
+    )
     return response.json()
 
 @router.get("/api/v1/investimentos/")
@@ -25,9 +29,22 @@ def listar_investimentos_por_cliente(cliente_id: str):
     investimentos_cliente = [inv for inv in investimentos if inv["cliente_id"] == cliente_id]
     return investimentos_cliente
 
+@router.put("/api/v1/investimentos/{investimento_id}")
+def atualizar_investimento(investimento_id: str, investimento_atualizado: dict):
+    response = requests.put(f"{DATA_SERVICE_URL}/{investimento_id}", json=investimento_atualizado)
+    if response.status_code >= 400:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail=response.json().get("detail")
+    )
+    return response.json()
 
 @router.delete("/api/v1/investimentos/{investimento_id}")
 def deletar_investimento(investimento_id: str):
     response = requests.delete(f"{DATA_SERVICE_URL}/{investimento_id}")
-    response.raise_for_status()
+    if response.status_code >= 400:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail=response.json().get("detail")
+    )
     return response.json()
