96e41ebd7669f809e8c2e76f5c68fd2825ab1c29
RafaelCespedes-br <rafael.cespedes@ibm.com>
2026-02-02 11:41:13 -0300

    Final gateway

--- investment_gateway_service/app/yahoo_service/yahoo_service.py
+++ investment_gateway_service/app/yahoo_service/yahoo_service.py
@@ -19,16 +19,3 @@ def obter_dados_mercado(ticker: str, periodo="1mo"):
         "variacao_percentual": round(variacao_percentual, 2),
         "periodo": periodo
     }
-
-
-def variacao_percentual(ticker: str, periodo="1y") -> float:
-    ativo = yf.Ticker(ticker)
-    hist = ativo.history(period=periodo)
-
-    if hist.empty:
-        return 0.0
-
-    preco_inicial = hist["Close"].iloc[0]
-    preco_final = hist["Close"].iloc[-1]
-
-    return round(((preco_final - preco_inicial) / preco_inicial) * 100, 2)
